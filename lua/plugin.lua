199ab1639062f27e783c325c96b16eb5c282a6cd
krvpb024 <krvpb024@protonmail.com>
2022-02-19 13:32:46 +0800

    init commit

---
+++ lua/plugin.lua
@@ -0,0 +1,26 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
+
+  use 'editorconfig/editorconfig-vim'
+
+  use 'itchyny/lightline.vim'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+end)
+
